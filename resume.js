5d4cf975406118e091ebbae37ec58779ce1397eb
kristinalagasca-torres <kristina@presagesecurity.com>
2020-12-26 22:21:22 -0500

    lol idk

--- resume.js
+++ resume.js
@@ -1,1 +1,6 @@
 var x = document.getElementsByClassName("terminal");
+
+$(document).ready(function()) {
+    console.log("o.o");
+    $(".prompt").html("guest@kristinalagasca.com ~/portfolio $ ");
+}
