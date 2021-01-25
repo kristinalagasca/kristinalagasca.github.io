38871456b3ef71c6103d395e82d7c2eaa774d5b5
kristinalagasca-torres <kristina@presagesecurity.com>
2021-01-25 11:02:04 -0500

    fixed accidentally deleted stuff

--- resume.js
+++ resume.js
@@ -1,5 +1,11 @@
+var x = document.getElementsByClassName("terminal");
 function terminalContent() {
     document.getElementById('t-content').innerHTML = '<p>guest@kristinalagasca.com ~/portfolio $ </p>';
+}
+
+$(document).ready(function()) {
+    console.log("o.o");
+    $(".prompt").html("guest@kristinalagasca.com ~/portfolio $ ");
 }
 
 print(terminalContent())
